a5c65213624b839bb2420f4f03a5f52734fbdf87
glink24 <guolinkai25@outlook.com>
2025-09-05 12:35:25 +0800

    feat: add cache

--- src/api/storage.ts
+++ src/api/storage.ts
@@ -1,9 +1,14 @@
-import { OncentDbClient } from "../lib";
+import { createIndexedDBStorage, OncentDbClient } from "../lib";
 import { getToken } from "./login";
 
+const storage = await createIndexedDBStorage();
 const repo = new OncentDbClient({
 	auth: getToken,
 	repoPrefix: "oncent-journal",
 	collectionName: "ledger",
+	cache: {
+		storage,
+		ttl: 24 * 60 * 60 * 1000, // 24hour
+	},
 });
 export const StorageAPI = repo;
