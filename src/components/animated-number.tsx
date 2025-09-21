1e2c03a3d06dfe739b029a15bad1f6bf26ede16a
glink25 <guolinkai25@outlook.com>
2025-09-21 16:51:46 +0800

    feat: animated number

---
+++ src/components/animated-number.tsx
@@ -0,0 +1,51 @@
+import { type HTMLMotionProps, motion, useSpring } from "framer-motion";
+import type React from "react";
+import { useEffect, useRef, useState } from "react";
+
+type AnimatedNumberProps = {
+	value: number;
+} & HTMLMotionProps<"span">;
+
+const AnimatedNumber: React.FC<AnimatedNumberProps> = ({
+	value,
+	...restProps
+}) => {
+	const previousValueRef = useRef(value);
+
+	// 根据传入的 value 自动判断小数位数
+	const precision = (value.toString().split(".")[1] || "").length;
+
+	const springValue = useSpring(previousValueRef.current, {
+		stiffness: 100,
+		damping: 20,
+	});
+
+	const [displayValue, setDisplayValue] = useState(value.toFixed(precision));
+
+	useEffect(() => {
+		springValue.set(value);
+
+		const unsubscribe = springValue.on("change", (latest) => {
+			// 检查 value 是否为整数
+			const isInteger = Number.isInteger(value);
+
+			if (isInteger) {
+				// 如果 value 是整数，只显示整数
+				setDisplayValue(Math.round(latest).toString());
+			} else {
+				// 如果 value 是小数，使用自动计算的精度
+				setDisplayValue(latest.toFixed(precision));
+			}
+		});
+
+		return () => unsubscribe();
+	}, [springValue, value, precision]);
+
+	useEffect(() => {
+		previousValueRef.current = value;
+	});
+
+	return <motion.span {...restProps}>{displayValue}</motion.span>;
+};
+
+export default AnimatedNumber;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,16 @@
+import dayjs from "dayjs";
+import { useMemo } from "react";
+import AnimatedNumber from "@/components/animated-number";
 import Ledger from "@/components/ledger";
 import Loading from "@/components/loading";
+import { amountToNumber } from "@/ledger/bill";
+import { useIntl } from "@/locale";
 import { useLedgerStore } from "@/store/ledger";
 import { cn } from "@/utils";
 
 export default function Page() {
 	const { bills, loading, sync } = useLedgerStore();
+	const t = useIntl();
 	const syncIcon =
 		sync === "wait"
 			? "icon-[mdi--cloud-minus-outline]"
@@ -13,10 +19,34 @@ export default function Page() {
 				: sync === "success"
 					? "icon-[mdi--cloud-check-outline]"
 					: "icon-[mdi--cloud-remove-outline]";
+
+	const todayBills = useMemo(() => {
+		const today: typeof bills = [];
+		const now = dayjs();
+		for (let index = 0; index < bills.length; index++) {
+			const bill = bills[index];
+			const billTime = dayjs.unix(bill.time / 1000);
+			if (billTime.isSame(now, "day")) {
+				today.push(bill);
+			} else {
+				break;
+			}
+		}
+		return today;
+	}, [bills]);
+
+	const todayAmount = useMemo(() => {
+		return todayBills.reduce((p, c) => {
+			return p + amountToNumber(c.amount) * (c.type === "income" ? 1 : -1);
+		}, 0);
+	}, [todayBills]);
 	return (
 		<div className="w-full h-full p-2 flex flex-col overflow-hidden">
 			<div className="flex flex-wrap">
-				<div className="bg-stone-800 h-20 w-full rounded-lg m-1 sm:flex-1 p-2"></div>
+				<div className="bg-stone-800 text-background relative h-20 w-full flex justify-end rounded-lg m-1 sm:flex-1 p-4">
+					<span className="absolute top-2 left-4">{t("Today")}</span>
+					<AnimatedNumber value={todayAmount} className="font-bold text-4xl " />
+				</div>
 			</div>
 			<div className="flex justify-between items-cente pl-7 pr-5 py-2">
 				<div>{loading && <Loading />}</div>
