9b62e9011786fe635d48309b888ecb755934c054
glink24 <guolinkai25@outlook.com>
2025-11-26 17:01:32 +0800

    feat: deprecated

--- src/components/category/form.tsx
+++ src/components/category/form.tsx
@@ -363,6 +363,7 @@ export default function CategoryEditForm({
                                 </div>
                                 <textarea
                                     className="w-full flex-1 border rounded-lg p-2"
+                                    placeholder={`<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12z"/></svg>`}
                                     onChange={(e) => {
                                         const svgText = e.currentTarget.value;
                                         if (
